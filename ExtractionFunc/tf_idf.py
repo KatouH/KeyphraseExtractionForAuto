9aa8044577ff7e8f1da1711e9c399fdf5aa0231f
HeD9817@gmail.com <HeD9817@gmail.com>
2020-02-01 16:24:25 +0800

    update tf_idf

--- ExtractionFunc/tf_idf.py
+++ ExtractionFunc/tf_idf.py
@@ -1,48 +1,71 @@
 from collections import defaultdict
 import math
 import jieba
+import logging
+import pickle
+import os
 
-stopword_list = []
-stopwords_file = open("../DataSet/stop_words.txt","r",encoding="utf-8")
-for stopword in stopwords_file.readlines():
-    stopword_list.append(stopword.strip())
+jieba.setLogLevel(logging.INFO)
 
-with open("../DataSet/dataset1.txt",'r',encoding="utf-8") as f:
-    comment_list = []
-    words = []
-    comments = f.readlines()
-    for comment in comments:
-        word_list = list(jieba.cut(comment,cut_all=False))
+class TFIDF:
+    __stopwords = list()
+    __corpus = list()
+    def __init__(self,stopwords_path="../DataSet/stop_words.txt",corpus_path="../DataSet/train1.txt"):
+        self.sw_file = open(stopwords_path,'r',encoding='utf-8')
+        self.cp_file = open(corpus_path,'r',encoding='utf-8')
+        print("Loading corpus...")
+        for stopword in self.sw_file.readlines():
+            self.__stopwords.append(stopword.strip())
+        for sentence in self.cp_file.readlines():
+            words = self.dataProcessing(sentence)
+            self.__corpus.append(words)
+        self.__docnums = len(self.__corpus)
+        print("Loading successfully")
+    def dataProcessing(self,sentence):
+        word_list = list(jieba.cut(sentence,cut_all=False))
+        words = []
         for word in word_list:
-            if word not in stopword_list and word != '\n':
+            if word not in self.__stopwords and word != '\n':
                 words.append(word)
-        comment_list.append(words)
-        words=[]
-#print(comment_list)
+        return words
+    def getTFIDF(self,sentence):
+        word_list = self.dataProcessing(sentence)
+        word_frequecny = defaultdict(int)
+        for word in word_list:
+            word_frequecny[word] += 1
+        word_tf = {}
+        for word in word_frequecny:
+            word_tf[word] = word_frequecny[word]/sum(word_frequecny.values())
 
-def tf_idf(comment):
-    word_frequecny = defaultdict(int)
-    for word in comment:
-        word_frequecny[word] += 1
-    
-    word_tf = {}
-    for word in word_frequecny:
-        word_tf[word] = word_frequecny[word]/sum(word_frequecny.values())
-    
-    doc_nums = len(comment_list)
-    word_idf = {}
-    word_doc = defaultdict(int)
-    for word in word_frequecny:
-        for comment in comment_list:
-            if word in comment:
-                word_doc[word] += 1
-        word_idf[word] = math.log(doc_nums/(word_doc[word]+1))
-    
-    word_tf_idf = {}
-    for word in word_frequecny:
-        word_tf_idf[word] = word_tf[word]*word_idf[word] 
-    
-    return word_tf_idf
+        word_idf = {}
+        word_doc = defaultdict(int)
+        for word in word_frequecny:
+            for doc in self.__corpus:
+                if word in doc:
+                    word_doc[word] += 1
+            word_idf[word] = math.log(self.__docnums/(word_doc[word]+1))
+        
+        word_tf_idf = {}
+        for word in word_frequecny:
+            word_tf_idf[word] = word_tf[word]*word_idf[word]
+        
+        return word_tf_idf
 
+def saveModel(obj,outfile="../Model/tfidf.pkl"):
+    with open(outfile,'w') as f:
+        pickle.dump(obj,f)
+
+def loadModel(infile="../Model/tfidf.pkl"):
+    if os.path.exists(infile):
+        with open(infile,'r') as f:
+            return pickle.load(f)
+    else:
+        return False
+
+#testing 
 if __name__ == "__main__":
-    print(tf_idf(comment_list[0]))
+    tfidf = loadModel()
+    if not tfidf:
+        tfidf = TFIDF()
+    print(tfidf.getTFIDF("空调不太凉，应该是小问题。"))
+    #print(tf_idf(comment_list[0]))
