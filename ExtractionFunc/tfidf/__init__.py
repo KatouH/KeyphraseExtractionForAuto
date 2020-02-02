af9e16ff925147685179da9d506b50c58cf518b0
HeD9817@gmail.com <HeD9817@gmail.com>
2020-02-02 17:56:09 +0800

    update tfidf set_corpus default mode

--- ExtractionFunc/tfidf/__init__.py
+++ ExtractionFunc/tfidf/__init__.py
@@ -7,7 +7,7 @@ default_tfidf = loadModel()
 
 tfidf_extraction = default_tfidf.getTFIDF
 
-def set_corpus(path,mode=0):
+def set_corpus(path,mode=1):
     default_tfidf.setCorpus(path,mode)
 def set_stopwords(path):
     default_tfidf.setStopWords(path)
