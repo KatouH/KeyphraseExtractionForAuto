a03fb965b0aff43fc99b635695009928c438ef15
HeD9817@gmail.com <HeD9817@gmail.com>
2020-02-05 20:52:19 +0800

    update bert

--- ExtractionFunc/extraction.py
+++ ExtractionFunc/extraction.py
@@ -1,6 +1,9 @@
 import tfidf
 import textrank
+from bert import modeling
+import tensorflow as tf
 
-print(tfidf.tfidf_extraction("要说不满意的话，那就是动力了，1.5自然吸气发动机对这款车有种小马拉大车的感觉。如今天气这么热，上路肯定得开空调，开了后动力明显感觉有些不给力不过空调制冷效果还是不错的。",5))
+s = tf.constant(1)
+# print(tfidf.tfidf_extraction("要说不满意的话，那就是动力了，1.5自然吸气发动机对这款车有种小马拉大车的感觉。如今天气这么热，上路肯定得开空调，开了后动力明显感觉有些不给力不过空调制冷效果还是不错的。",5))
 
-print(textrank.textrank_extraction("要说不满意的话，那就是动力了，1.5自然吸气发动机对这款车有种小马拉大车的感觉。如今天气这么热，上路肯定得开空调，开了后动力明显感觉有些不给力不过空调制冷效果还是不错的。",topK=5))
+# print(textrank.textrank_extraction("要说不满意的话，那就是动力了，1.5自然吸气发动机对这款车有种小马拉大车的感觉。如今天气这么热，上路肯定得开空调，开了后动力明显感觉有些不给力不过空调制冷效果还是不错的。",topK=5))
